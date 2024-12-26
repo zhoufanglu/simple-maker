fd58e84f137a340a6b458a05181d46de5c3ea6cd
wrk <webwkun@163.com>
2024-12-26 15:23:16 +0800

    [需] 列表添加双击删除当前已被选中的图片

--- frontend/src/views/home/components/Content.vue
+++ frontend/src/views/home/components/Content.vue
@@ -1,93 +1,89 @@
 <script setup lang="ts">
-  import Draggable from 'vuedraggable'
-  import { RankingItem } from '../types'
-  import Motion from '@/components/motion'
-  import { useMessage } from 'naive-ui'
-  import { useHomeStore } from '@/store/home'
-  import InputTextEnter from '@/components/InputTextEnter.vue'
-  import { defineEmits } from 'vue'
+import Draggable from 'vuedraggable'
+import { RankingItem } from '../types'
+import Motion from '@/components/motion'
+import { useMessage } from 'naive-ui'
+import { useHomeStore } from '@/store/home'
+import InputTextEnter from '@/components/InputTextEnter.vue'
+import { defineEmits } from 'vue'
 
-  const message = useMessage()
-  const homeStore = useHomeStore()
+const message = useMessage()
+const homeStore = useHomeStore()
 
-  const rankingRows = ref<RankingItem[]>([])
-  const defaultColorList = [
-    '#F97875', // 纯红
-    '#FF7F00', // 橙色
-    '#73D13D', // 纯绿
-    '#69C0FF', // 纯蓝
-    '#80bcbc', // 青色
-    '#8B00FF', // 紫罗兰
-    '#FF1493', // 深粉色
-    '#FFD700', // 金色
-    '#00FA9A', // 薄荷绿
-    '#9dc3b3', // 薄荷绿
-  ]
-  const defaultLevelList = ['S', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
-  const defaultImgList = [
-    'https://api.dicebear.com/7.x/adventurer/svg?seed=1',
-    'https://api.dicebear.com/7.x/adventurer/svg?seed=2',
-    'https://api.dicebear.com/7.x/adventurer/svg?seed=3',
-    'https://api.dicebear.com/7.x/adventurer/svg?seed=4',
-  ]
+const rankingRows = ref<RankingItem[]>([])
+const defaultColorList = [
+  '#F97875', // 纯红
+  '#FF7F00', // 橙色
+  '#73D13D', // 纯绿
+  '#69C0FF', // 纯蓝
+  '#80bcbc', // 青色
+  '#8B00FF', // 紫罗兰
+  '#FF1493', // 深粉色
+  '#FFD700', // 金色
+  '#00FA9A', // 薄荷绿
+  '#9dc3b3', // 薄荷绿
+]
+const defaultLevelList = ['S', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+const defaultImgList = [
+  'https://api.dicebear.com/7.x/adventurer/svg?seed=1',
+  'https://api.dicebear.com/7.x/adventurer/svg?seed=2',
+  'https://api.dicebear.com/7.x/adventurer/svg?seed=3',
+  'https://api.dicebear.com/7.x/adventurer/svg?seed=4',
+]
 
-  rankingRows.value = []
+rankingRows.value = []
 
-  initRankingRows()
-  // test
-  /* rankingRows.value[0].items.push({ path: defaultImgList[0] })
-  rankingRows.value[0].items.push({ path: defaultImgList[1] }) */
+initRankingRows()
+// test
+/* rankingRows.value[0].items.push({ path: defaultImgList[0] })
+rankingRows.value[0].items.push({ path: defaultImgList[1] }) */
 
-  function initRankingRows() {
-    for (let i = 0; i < 4; i++) {
-      rankingRows.value.push({
-        levelName: defaultLevelList[i],
-        items: [],
-        bgColor: defaultColorList[i],
-      })
-    }
-  }
-
-  function handleCreate() {
-    const len = rankingRows.value.length
-    if (len > 9) {
-      message.info('最多10个！')
-      return
-    }
+function initRankingRows() {
+  for (let i = 0; i < 4; i++) {
     rankingRows.value.push({
-      levelName: defaultLevelList[len],
+      levelName: defaultLevelList[i],
       items: [],
-      bgColor: defaultColorList[len],
+      bgColor: defaultColorList[i],
     })
   }
+}
 
-  const emits = defineEmits(['handleDelRow'])
-
-  const handleDelRow = (index: number) => {
-    console.log(rankingRows.value[index])
-    emits('handleDelRow', rankingRows.value[index])
-    rankingRows.value.splice(index, 1)
+function handleCreate() {
+  const len = rankingRows.value.length
+  if (len > 9) {
+    message.info('最多10个！')
+    return
   }
+  rankingRows.value.push({
+    levelName: defaultLevelList[len],
+    items: [],
+    bgColor: defaultColorList[len],
+  })
+}
 
-  const hexToRgba = (hex: string, alpha = 0.2) => {
-    hex = hex.replace('#', '')
-    const r = parseInt(hex.substring(0, 2), 16)
-    const g = parseInt(hex.substring(2, 4), 16)
-    const b = parseInt(hex.substring(4, 6), 16)
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`
-  }
+const emits = defineEmits(['handleDelRow', 'dbRemoveClick'])
+
+const handleDelRow = (index: number) => {
+  console.log(rankingRows.value[index])
+  emits('handleDelRow', rankingRows.value[index])
+  rankingRows.value.splice(index, 1)
+}
+const dbRemoveClick = (img: RankingItem, index: number, imgIndex: number) => {
+  rankingRows.value[index].items.splice(imgIndex, 1)
+  emits('dbRemoveClick', img)
+}
+const hexToRgba = (hex: string, alpha = 0.2) => {
+  hex = hex.replace('#', '')
+  const r = parseInt(hex.substring(0, 2), 16)
+  const g = parseInt(hex.substring(2, 4), 16)
+  const b = parseInt(hex.substring(4, 6), 16)
+  return `rgba(${r}, ${g}, ${b}, ${alpha})`
+}
 </script>
 <template>
   <div v-auto-animate class="content">
-    <Draggable
-      v-auto-animate
-      style="height: auto"
-      handle=".handle"
-      ghost-class="ghost"
-      animation="200"
-      :list="rankingRows"
-      item-key="levelName"
-    >
+    <Draggable v-auto-animate style="height: auto" handle=".handle" ghost-class="ghost" animation="200"
+      :list="rankingRows" item-key="levelName">
       <template #item="{ element: rank, index }">
         <ul class="rank-row">
           <!--?level-item-->
@@ -98,30 +94,22 @@
           </li>
           <!--?img-list-->
           <!--!行内img拖动-->
-          <draggable
-            :style="{
-              width:
-                homeStore.modeType === 'edit' ? 'calc(100% - 150px)' : 'calc(100% - 150px + 16px)',
-              borderColor: hexToRgba(rank.bgColor),
-            }"
-            class="img-row"
-            ghost-class="ghost"
-            animation="200"
-            :list="rank.items"
-            group="img-row"
-            itemKey="path"
-          >
-            <template #item="{ element: img }">
-              <n-image :src="img.path" preview-disabled width="80" class="img-item"> </n-image>
+          <draggable :style="{
+            width:
+              homeStore.modeType === 'edit' ? 'calc(100% - 150px)' : 'calc(100% - 150px + 16px)',
+            borderColor: hexToRgba(rank.bgColor),
+          }" class="img-row" ghost-class="ghost" animation="200" :list="rank.items" group="img-row" itemKey="path">
+            <template #item="{ element: img, index: imgIndex }">
+              <n-image @dblclick="dbRemoveClick(img, index, imgIndex)" :src="img.path" preview-disabled width="80"
+                class="img-item">
+              </n-image>
             </template>
           </draggable>
           <!--?action-->
           <Motion v-if="homeStore.modeType === 'edit'">
             <li class="action-item drag-handle">
               <i class="iconfont fa fa-align-justify handle">&#xe6c4;</i>
-              <i class="iconfont fa fa-align-justify handle" @click="handleDelRow(index)"
-                >&#xe616;</i
-              >
+              <i class="iconfont fa fa-align-justify handle" @click="handleDelRow(index)">&#xe616;</i>
             </li>
           </Motion>
         </ul>
@@ -134,92 +122,104 @@
 </template>
 
 <style scoped lang="scss">
-  .content {
-    background-color: white;
-    border-radius: 8px;
-    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
-    padding: 16px;
-    box-sizing: border-box;
-    //transition: all 0.3s ease;
-    .rank-row {
+.content {
+  background-color: white;
+  border-radius: 8px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+  padding: 16px;
+  box-sizing: border-box;
+
+  //transition: all 0.3s ease;
+  .rank-row {
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+
+    .level-item {
+      flex-shrink: 0;
+      height: 100px;
+      width: 100px;
+      @include vertical-center;
+      font-size: 24px;
+      font-weight: bold;
+      color: white;
+      border-radius: 8px;
+      margin-right: 8px;
+
+      .level-name {}
+    }
+
+    .img-row {
+      background-color: #fafafa;
+      min-height: 100px;
+      // width: calc(100% - 150px - 16px);
+      box-sizing: border-box;
+      border-radius: 8px;
+      padding: 8px;
       display: flex;
       align-items: center;
-      margin-bottom: 10px;
-      .level-item {
-        flex-shrink: 0;
-        height: 100px;
-        width: 100px;
-        @include vertical-center;
-        font-size: 24px;
-        font-weight: bold;
-        color: white;
+      flex-wrap: wrap;
+      gap: 8px;
+      border: 2px dashed #e8e8e8;
+
+      // transition: all 0.3s ease;
+      .img-item {
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        cursor: grab;
+        // padding: 8px;
         border-radius: 8px;
-        margin-right: 8px;
-        .level-name {
-        }
-      }
-      .img-row {
-        background-color: #fafafa;
-        min-height: 100px;
-        // width: calc(100% - 150px - 16px);
         box-sizing: border-box;
-        border-radius: 8px;
-        padding: 8px;
-        display: flex;
-        align-items: center;
-        flex-wrap: wrap;
-        gap: 8px;
-        border: 2px dashed #e8e8e8;
-        // transition: all 0.3s ease;
-        .img-item {
-          box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-          cursor: grab;
-          // padding: 8px;
-          border-radius: 8px;
-          box-sizing: border-box;
-          background: white;
-          height: 80px;
-          width: 80px;
-          &:hover {
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
-          }
-        }
-      }
-      .action-item {
+        background: white;
         height: 80px;
-        padding: 0 10px;
-        @include vertical-center;
-        flex-direction: column;
-        transition: all 0.3s ease;
-        i {
-          font-size: 20px;
-          cursor: pointer;
-          &:hover {
-            color: #5386ed;
-          }
-        }
-        i:last-child {
-          font-size: 24px;
-          margin-top: 6px;
+        width: 80px;
+
+        &:hover {
+          box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
         }
       }
     }
-    .add-action {
-      width: 100%;
-      height: 30px;
-      border: 2px dashed #e8e8e8;
-      border-radius: 4px;
+
+    .action-item {
+      height: 80px;
+      padding: 0 10px;
       @include vertical-center;
+      flex-direction: column;
       transition: all 0.3s ease;
-      &:hover {
-        color: #5386ed;
-        border: 2px dashed #5386ed;
-        cursor: pointer;
-      }
+
       i {
         font-size: 20px;
         cursor: pointer;
+
+        &:hover {
+          color: #5386ed;
+        }
+      }
+
+      i:last-child {
+        font-size: 24px;
+        margin-top: 6px;
       }
     }
   }
+
+  .add-action {
+    width: 100%;
+    height: 30px;
+    border: 2px dashed #e8e8e8;
+    border-radius: 4px;
+    @include vertical-center;
+    transition: all 0.3s ease;
+
+    &:hover {
+      color: #5386ed;
+      border: 2px dashed #5386ed;
+      cursor: pointer;
+    }
+
+    i {
+      font-size: 20px;
+      cursor: pointer;
+    }
+  }
+}
 </style>
--- frontend/src/views/home/index.vue
+++ frontend/src/views/home/index.vue
@@ -1,50 +1,55 @@
 <script setup lang="ts">
-  import Title from '@/views/home/components/Title.vue'
-  import ChooseImages from '@/views/home/components/ChooseImages.vue'
-  import Content from '@/views/home/components/Content.vue'
-  import Header from '@/views/home/components/Header.vue'
-  import { RankingItem } from '@/views/home/types'
-  import { useHomeStore } from '@/store/home'
-  // @ts-ignore
-  import domtoimage from 'dom-to-image'
-  import { useMessage } from 'naive-ui'
+import Title from '@/views/home/components/Title.vue'
+import ChooseImages from '@/views/home/components/ChooseImages.vue'
+import Content from '@/views/home/components/Content.vue'
+import Header from '@/views/home/components/Header.vue'
+import { RankingItem, ImgItem } from '@/views/home/types'
+import { useHomeStore } from '@/store/home'
+// @ts-ignore
+import domtoimage from 'dom-to-image'
+import { useMessage } from 'naive-ui'
 
-  const chooseImagesRef = ref<InstanceType<typeof ChooseImages>>()
-  const homeStore = useHomeStore()
+const chooseImagesRef = ref<InstanceType<typeof ChooseImages>>()
+const homeStore = useHomeStore()
 
-  const handleDelRow = (row: RankingItem) => {
-    chooseImagesRef.value.addImages(row.items)
+const handleDelRow = (row: RankingItem) => {
+  console.log('删除行', row);
+
+  chooseImagesRef.value.addImages(row.items)
+}
+const dbDelCard = (img: ImgItem) => {
+  chooseImagesRef.value.addImages([{ ...img }])
+  // row.items.splice(index, 1)
+}
+// ? 模拟一个下面的占位box
+const pinImgsBoxHeight = ref(0)
+const handleImageBoxHeightChange = (height: number, isPin: boolean) => {
+  pinImgsBoxHeight.value = isPin ? height + 10 : 0
+}
+
+const previewBoxRef = ref<HTMLElement>()
+const titleRef = ref<InstanceType<typeof Title>>(null)
+const message = useMessage()
+const downloadLoading = ref(false)
+const handleDownload = async () => {
+  const element = previewBoxRef.value
+  downloadLoading.value = true
+  try {
+    const dataUrl = await domtoimage.toPng(element, {
+      style: { background: 'transparent' }, // 保持透明背景
+    })
+    const link = document.createElement('a')
+    link.href = dataUrl
+    link.download = `${titleRef.value.title}.png` // 下载文件名
+    link.click()
+    message.success('导出成功')
+  } catch (error) {
+    message.error(`导出失败：${error}`)
+    console.error('生成图片失败', error)
+  } finally {
+    downloadLoading.value = false
   }
-
-  // ? 模拟一个下面的占位box
-  const pinImgsBoxHeight = ref(0)
-  const handleImageBoxHeightChange = (height: number, isPin: boolean) => {
-    pinImgsBoxHeight.value = isPin ? height + 10 : 0
-  }
-
-  const previewBoxRef = ref<HTMLElement>()
-  const titleRef = ref<InstanceType<typeof Title>>(null)
-  const message = useMessage()
-  const downloadLoading = ref(false)
-  const handleDownload = async () => {
-    const element = previewBoxRef.value
-    downloadLoading.value = true
-    try {
-      const dataUrl = await domtoimage.toPng(element, {
-        style: { background: 'transparent' }, // 保持透明背景
-      })
-      const link = document.createElement('a')
-      link.href = dataUrl
-      link.download = `${titleRef.value.title}.png` // 下载文件名
-      link.click()
-      message.success('导出成功')
-    } catch (error) {
-      message.error(`导出失败：${error}`)
-      console.error('生成图片失败', error)
-    } finally {
-      downloadLoading.value = false
-    }
-  }
+}
 </script>
 <template>
   <div class="p-home">
@@ -54,18 +59,12 @@
       <div ref="previewBoxRef" class="preview-box">
         <Title ref="titleRef"></Title>
         <div class="container">
-          <content @handle-del-row="handleDelRow"></content>
-          <choose-images
-            v-show="homeStore.modeType === 'edit'"
-            ref="chooseImagesRef"
-            @handle-image-box-height-change="handleImageBoxHeightChange"
-          ></choose-images>
-          <div
-            class="pin-box"
-            :style="{
-              height: pinImgsBoxHeight + 'px',
-            }"
-          ></div>
+          <content @handle-del-row="handleDelRow" @db-remove-click="dbDelCard"></content>
+          <choose-images v-show="homeStore.modeType === 'edit'" ref="chooseImagesRef"
+            @handle-image-box-height-change="handleImageBoxHeightChange"></choose-images>
+          <div class="pin-box" :style="{
+            height: pinImgsBoxHeight + 'px',
+          }"></div>
         </div>
       </div>
     </n-spin>
@@ -73,26 +72,28 @@
 </template>
 
 <style scoped lang="scss">
-  .p-home {
-    height: 100vh;
+.p-home {
+  height: 100vh;
+  width: 100%;
+  box-sizing: border-box;
+  padding: 0 20px 20px 20px;
+  overflow-y: auto;
+  position: relative;
+
+  //border: solid 1px red;
+  .container {
+    // border: dashed 1px black;
+    border-radius: 10px;
+    // height: 100%;
     width: 100%;
     box-sizing: border-box;
-    padding: 0 20px 20px 20px;
+    background-color: #f5f5f5;
+    padding: 10px;
     overflow-y: auto;
-    position: relative;
-    //border: solid 1px red;
-    .container {
-      // border: dashed 1px black;
-      border-radius: 10px;
-      // height: 100%;
-      width: 100%;
-      box-sizing: border-box;
-      background-color: #f5f5f5;
-      padding: 10px;
-      overflow-y: auto;
-    }
-    .pin-box {
-      // border: solid 1px red;
-    }
   }
+
+  .pin-box {
+    // border: solid 1px red;
+  }
+}
 </style>
